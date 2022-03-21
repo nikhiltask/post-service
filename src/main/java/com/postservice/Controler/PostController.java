21bc712506061aabee370ef1f200549c9a107ca7
Nikhil Arun Nidhan <nikhilar@maveric-systems.com>
2022-03-21 21:11:46 +0530

    post Method

--- src/main/java/com/postservice/Controler/PostController.java
+++ src/main/java/com/postservice/Controler/PostController.java
@@ -1,12 +1,24 @@
 package com.postservice.Controler;
 
+import com.postservice.Model.PostModel;
 import com.postservice.Service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 public class PostController {
 
     @Autowired
     private PostService postService;
+
+    @PostMapping("/posts")
+    public ResponseEntity<PostModel> userPost(@RequestBody @Valid PostModel postModel){
+        return  new ResponseEntity<>(postService.userPost(postModel), HttpStatus.ACCEPTED);
+    }
 }
