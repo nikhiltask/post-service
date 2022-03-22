8985a49b270dd249d4f66ddb3e0223d9a3f2256d
Prasad Gowda K N <prasadgk@maveric-systems.com>
2022-03-22 15:19:48 +0530

    Add Get all user Method

--- src/main/java/com/postservice/Controler/PostController.java
+++ src/main/java/com/postservice/Controler/PostController.java
@@ -1,12 +1,23 @@
 package com.postservice.Controler;
 
+import com.postservice.Model.PostModel;
 import com.postservice.Service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class PostController {
 
     @Autowired
     private PostService postService;
+
+    @GetMapping("/posts")
+    public ResponseEntity<List<PostModel>> allUser(){
+        return new ResponseEntity<>(postService.allUser(), HttpStatus.ACCEPTED);
+    }
 }
--- src/main/java/com/postservice/Service/PostService.java
+++ src/main/java/com/postservice/Service/PostService.java
@@ -1,12 +1,18 @@
 package com.postservice.Service;
 
+import com.postservice.Model.PostModel;
 import com.postservice.Repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PostService {
 
     @Autowired
     private PostRepository postRepository;
+    public List<PostModel> allUser(){
+        return postRepository.findAll();
+    }
 }
